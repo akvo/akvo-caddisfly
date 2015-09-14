4b834222a98bbcef8f407e025a9366d8e5c4ee45
ldporsius <linda.d.porsius@gmail.com>
2015-09-14 14:25:09 +0200

    Vizualise progress of time lapse test by drawing a strip test look-alike.

--- strip-test/app/src/main/java/org/akvo/akvoqr/ui/ProgressIndicatorView.java
+++ strip-test/app/src/main/java/org/akvo/akvoqr/ui/ProgressIndicatorView.java
@@ -2,19 +2,21 @@ package org.akvo.akvoqr.ui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
-
-import org.akvo.akvoqr.R;
 
 /**
  * Created by linda on 9/4/15.
  */
 public class ProgressIndicatorView extends View {
 
-    private Paint lightbluePaint;
-    private Paint mediumbluePaint;
+    private Paint strokePaint;
+    private Paint neutralPaint;
+    private Paint fillPaint;
+    private Paint stripPaint;
     private int totalSteps = 1;
     private int stepsTaken = 0;
     private int distance;
@@ -30,12 +32,20 @@ public class ProgressIndicatorView extends View {
     public ProgressIndicatorView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        this.lightbluePaint = new Paint();
-        lightbluePaint.setColor(context.getResources().getColor(R.color.lightblue));
-        lightbluePaint.setAntiAlias(true);
+        this.strokePaint = new Paint();
+        strokePaint.setColor(Color.BLACK);
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setStrokeWidth(3);
+        strokePaint.setAntiAlias(true);
 
-        mediumbluePaint = new Paint(lightbluePaint);
-        mediumbluePaint.setColor(context.getResources().getColor(R.color.mediumblue));
+        fillPaint = new Paint(strokePaint);
+        fillPaint.setStyle(Paint.Style.FILL);
+
+        neutralPaint = new Paint(fillPaint);
+        neutralPaint.setColor(Color.LTGRAY);
+
+        stripPaint = new Paint(fillPaint);
+        stripPaint.setColor(Color.WHITE);
 
     }
 
@@ -58,19 +68,28 @@ public class ProgressIndicatorView extends View {
     public void onDraw(Canvas canvas)
     {
         canvas.drawARGB(255, 0, 0, 0);
-        distance = canvas.getHeight()/totalSteps;
+        distance = (int) Math.round(canvas.getHeight() * 0.75)/totalSteps;
+
+        canvas.drawRect(canvas.getWidth()/2 - 20, 20, canvas.getWidth()/2 + 20, canvas.getHeight() + 20, stripPaint);
 
         for(int i=0;i < totalSteps;i++) {
+            Rect rect = new Rect(canvas.getWidth()/2 - 18,
+                    (distance * i + distance/2),
+                    canvas.getWidth()/2 + 18,
+                    (distance * i) + distance/2 + 36);
+
             if(i < stepsTaken)
             {
-                //canvas.drawRect(distance*(i-1) + (canvas.getWidth() / 3) * 2, canvas.getHeight()/2 - 5, distance * i, canvas.getHeight()/2 + 5, mediumbluePaint);
-                canvas.drawCircle(canvas.getWidth() / 2, distance * i + distance/2 , canvas.getWidth() / 3, mediumbluePaint);
+               // canvas.drawCircle(canvas.getWidth() / 2, distance * i + distance/2 , canvas.getWidth() / 3, fillPaint);
 
+                canvas.drawRect(rect, fillPaint);
             }
             else
             {
-                //canvas.drawRect(distance*(i-1) + (canvas.getHeight() / 3) * 2, canvas.getHeight()/2 - 5, distance * i, canvas.getHeight()/2 + 5, lightbluePaint);
-                canvas.drawCircle(canvas.getWidth() / 2, distance*i + distance/2 , canvas.getWidth()/3, lightbluePaint);
+                //canvas.drawCircle(canvas.getWidth() / 2, distance*i + distance/2 , canvas.getWidth()/3, strokePaint);
+
+                canvas.drawRect(rect, neutralPaint);
+                canvas.drawRect(rect, strokePaint);
             }
 
         }
