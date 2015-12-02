6169e64756b32d5b5d508c7cf70fa68223845c8d
ldporsius <linda.d.porsius@gmail.com>
2015-12-02 19:01:13 +0100

    [#37] Basic structure for uncaught exception handling.

--- strip-test/app/src/main/java/org/akvo/akvoqr/util/App.java
+++ strip-test/app/src/main/java/org/akvo/akvoqr/util/App.java
@@ -2,6 +2,7 @@ package org.akvo.akvoqr.util;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 
 /**
  * Created by linda on 8/19/15.
@@ -13,7 +14,31 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
+
+        // Setup handler for uncaught exceptions.
+        Thread.setDefaultUncaughtExceptionHandler (new Thread.UncaughtExceptionHandler()
+        {
+            @Override
+            public void uncaughtException (Thread thread, Throwable e)
+            {
+                handleUncaughtException (thread, e);
+            }
+        });
     }
+
+    public void handleUncaughtException (Thread thread, Throwable e)
+    {
+        e.printStackTrace(); // not all Android versions will print the stack trace automatically
+
+        Intent intent = new Intent ();
+        intent.setAction ("com.mydomain.SEND_LOG"); // see step 5.
+        intent.putExtra(Constant.ERROR, e.getMessage());
+        intent.setFlags (Intent.FLAG_ACTIVITY_NEW_TASK); // required when starting from Application
+        startActivity (intent);
+
+        System.exit(1); // kill off the crashed app
+    }
+
 
     public static Context getMyApplicationContext()
     {
