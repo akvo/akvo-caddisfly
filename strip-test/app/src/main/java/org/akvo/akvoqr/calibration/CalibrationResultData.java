cbaed3b681ef65d90ba4bd31af2ec3e22d319132
Mark Tiele Westra <m.t.westra@akvo.org>
2016-01-06 09:25:04 +0100

    [#11] include total E94 error in quality report

--- strip-test/app/src/main/java/org/akvo/akvoqr/calibration/CalibrationResultData.java
+++ strip-test/app/src/main/java/org/akvo/akvoqr/calibration/CalibrationResultData.java
@@ -14,10 +14,12 @@ public class CalibrationResultData {
     public Mat calibratedImage;
     public double meanE94;
     public double maxE94;
+    public double totalE94;
 
-    public CalibrationResultData(Mat img, double meanE94, double maxE94){
+    public CalibrationResultData(Mat img, double meanE94, double maxE94, double totalE94){
         this.calibratedImage = img;
         this.meanE94 = meanE94;
         this.maxE94 = maxE94;
+        this.totalE94 = totalE94;
     }
 }
