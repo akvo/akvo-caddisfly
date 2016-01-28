3e3563404d74c37ab492ab2c2cd1e6b6b1f7c2f1
nhternup <nhternup@users.noreply.github.com>
2016-01-28 20:07:20 +0530

    Test config changes, Third party notices

--- caddisfly-app/app/src/main/java/org/akvo/caddisfly/model/TestInfo.java
+++ caddisfly-app/app/src/main/java/org/akvo/caddisfly/model/TestInfo.java
@@ -36,6 +36,7 @@ public class TestInfo {
     private final CaddisflyApp.TestType testType;
     private final ArrayList<Integer> dilutions;
     private final boolean requiresCalibration;
+    private boolean allInteger = true;
     private boolean isDiagnostic;
     private boolean mIsDirty;
     private int monthsValid = 12;
@@ -67,6 +68,10 @@ public class TestInfo {
 
             Swatch swatch = new Swatch(((int) (Double.valueOf(range) * 10)) / 10f,
                     Color.TRANSPARENT, defaultColor);
+
+            if (allInteger && swatch.getValue() % 1 != 0) {
+                allInteger = false;
+            }
             addSwatch(swatch);
 
         }
@@ -187,5 +192,9 @@ public class TestInfo {
 
     public int getMonthsValid() {
         return monthsValid;
+    }
+
+    public boolean hasDecimalPlace() {
+        return !allInteger;
     }
 }
