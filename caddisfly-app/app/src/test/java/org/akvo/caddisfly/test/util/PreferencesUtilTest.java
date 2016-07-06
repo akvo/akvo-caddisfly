e4b8aa048129557f5786b3af567c5a83345052fa
nhternup <nhternup@users.noreply.github.com>
2016-07-06 10:42:39 +0530

    Change tests package name

---
+++ caddisfly-app/app/src/test/java/org/akvo/caddisfly/test/util/PreferencesUtilTest.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright (C) Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo Caddisfly
+ *
+ *  Akvo Caddisfly is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo Caddisfly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+package org.akvo.caddisfly.test.util;
+
+import junit.framework.TestCase;
+
+import org.akvo.caddisfly.helper.TestConfigHelper;
+
+@SuppressWarnings({"unused", "EmptyMethod"})
+public class PreferencesUtilTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+        ClassUtil.assertUtilityClassWellDefined(TestConfigHelper.class);
+    }
+
+    public void testGetBoolean() throws Exception {
+
+    }
+
+    public void testSetBoolean() throws Exception {
+
+    }
+
+    public void testGetInt() throws Exception {
+
+    }
+
+    public void testGetInt1() throws Exception {
+
+    }
+
+    public void testSetInt() throws Exception {
+
+    }
+
+    public void testSetInt1() throws Exception {
+
+    }
+
+    public void testSetDouble() throws Exception {
+
+    }
+
+    public void testGetLong() throws Exception {
+
+    }
+
+    public void testGetLong1() throws Exception {
+
+    }
+
+    public void testSetLong() throws Exception {
+
+    }
+
+    public void testGetString() throws Exception {
+
+    }
+
+    public void testRemoveKey() throws Exception {
+
+    }
+
+    public void testContains() throws Exception {
+
+    }
+
+    public void testGetDouble() throws Exception {
+
+    }
+}
